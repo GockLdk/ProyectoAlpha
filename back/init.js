8655a8b71139551c2395cdf4c197c34d4ed9bedf
GockLdk <gock.proteco@gmail.com>
2019-12-14 14:52:54 -0600

    Agregando a init.js el script del preloader :stuck_out_tongue_closed_eyes:

--- back/init.js
+++ back/init.js
@@ -13,3 +13,8 @@
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems,{});
    });
+
+//JS para mostrar preloader mientras carga 
+  $(window).on("load",function () {
+    $(".loader-wrapper").fadeOut("slow");
+  });
