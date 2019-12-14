c3a69f605c9a2b9f8099ceb5ee5cda9a48e05d6f
GockLdk <gock.proteco@gmail.com>
2019-12-14 15:01:55 -0600

    No funciono lo de Preloader asi que restaure como se tenia :sweat_smile:

--- back/init.js
+++ back/init.js
@@ -14,7 +14,3 @@
     var instances = M.Materialbox.init(elems,{});
    });
 
-//JS para mostrar preloader mientras carga 
-  $(window).on("load",function () {
-    $(".loader-wrapper").fadeOut("slow");
-  });
